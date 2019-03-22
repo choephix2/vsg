601da8059a30146088cec713a99f5f89cf3c29c6
Stefan <i@thechoephix.com>
2019-03-22 14:17:38 +0000

    automation changes

--- frontend_fix.sh
+++ frontend_fix.sh
@@ -1,11 +1,13 @@
 for D in `find ./frontend/* -maxdepth 0 -type d`
 do
-  echo -n "${D}"
-  cp "frontend_fix/game.html" "${D}/game.html"
-  mv "${D}/c2runtime.js" "${D}/analytics.js" 2>% || echo -n " - - - (c2runtime.js not found)"
-  echo ""
-#   rm "${D}/index.html"
-#   rm "${D}/sw.js"
-#   rm "${D}/offline.js"
-#   rm "${D}/offlineClient.js"
+  echo -n ${D}
+  cp frontend_fix/game.html ${D}/game.html
+  echo '       del:'
+  mv ${D}/c2runtime.js ${D}/analytics.js 2>% && echo -n ' .js'
+  rm ${D}/index.html                     2>% && echo -n ' index'
+  rm ${D}/sw.js                          2>% && echo -n ' sw'
+  rm ${D}/offline.js                     2>% && echo -n ' off'
+  rm ${D}/offlineClient.js               2>% && echo -n ' offc'
+  rm ${D}/icon-*.png                     2>% && echo -n ' icons'
+  echo ''
 done
