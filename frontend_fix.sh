99f2b1f7e5cb1dbe3a8e19a75a402d947d0cb351
Stefan <i@thechoephix.com>
2019-03-22 14:23:28 +0000

    clnup & fix

--- frontend_fix.sh
+++ frontend_fix.sh
@@ -2,12 +2,12 @@ for D in `find ./frontend/* -maxdepth 0 -type d`
 do
   echo -n ${D}
   cp frontend_fix/game.html ${D}/game.html
-  echo '       del:'
+  echo -n '       del:'
   mv ${D}/c2runtime.js ${D}/analytics.js 2>% && echo -n ' .js'
   rm ${D}/index.html                     2>% && echo -n ' index'
-  rm ${D}/sw.js                          2>% && echo -n ' sw'
-  rm ${D}/offline.js                     2>% && echo -n ' off'
-  rm ${D}/offlineClient.js               2>% && echo -n ' offc'
+#   rm ${D}/sw.js                          2>% && echo -n ' sw'
+#   rm ${D}/offline.js                     2>% && echo -n ' off'
+#   rm ${D}/offlineClient.js               2>% && echo -n ' offc'
   rm ${D}/icon-*.png                     2>% && echo -n ' icons'
   echo ''
 done
